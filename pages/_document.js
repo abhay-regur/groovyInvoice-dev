c9301521e510eac106efd6772c973b7216b54827
Abhay K. Keluskar <abhaykeluskar@live.com>
2022-11-02 11:44:21 +0530

    Fixed Tag Error

--- pages/_document.js
+++ pages/_document.js
@@ -4,10 +4,9 @@ export default function Document() {
     return (
         <Html>
             <Head>
-                <link rel="preconnect" href="https://fonts.googleapis.com">
-                    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-                    <link href="https://fonts.googleapis.com/css2?family=Mulish:wght@400;700;800&display=swap" rel="stylesheet" />
-                </link>
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+                <link href="https://fonts.googleapis.com/css2?family=Mulish:wght@400;700;800&display=swap" rel="stylesheet" />
             </Head>
             <body>
                 <Main />
