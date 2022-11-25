5b588e7807055e156c4a55b4fbb490b23bfaa84b
Abhay K. Keluskar <abhaykeluskar@live.com>
2022-11-25 11:21:25 +0530

    Navbar icon Update

--- components/navitem.js
+++ components/navitem.js
@@ -1,11 +1,11 @@
 import Link from "next/link";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import style from '../styles/navbar.module.scss';
+
 
 export default function NavItem({ text, href, icon, active }) {
     return (
         <Link className={`nav__item ${active ? "active" : ""}`} href={`${href}`}>
-            <a><FontAwesomeIcon className={`green`} icon={icon} />
+            <a>{icon}
                 <span className={`${style.nav_menuName}`}>{text}</span>
             </a>
         </Link>
