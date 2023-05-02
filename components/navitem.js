82dd6939500efa916f34ed78257ec768290fc6f7
Abhay K. Keluskar <abhaykeluskar@live.com>
2023-05-02 20:09:27 +0530

    NextJS Version Upgrade: 12.3.1 to 13.3.1 and Tag Changes

--- components/navitem.js
+++ components/navitem.js
@@ -5,9 +5,8 @@ import style from '../styles/navbar.module.scss';
 export default function NavItem({ text, href, icon, active }) {
     return (
         <Link className={`nav__item ${active ? "active" : ""}`} href={`${href}`}>
-            <a>{icon}
-                <span className={`${style.nav_menuName}`}>{text}</span>
-            </a>
+            {icon}
+            <span className={`${style.nav_menuName}`}>{text}</span>
         </Link>
     )
 }
