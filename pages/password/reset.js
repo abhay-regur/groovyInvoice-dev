32423c053926fb7d68216fcc625985e868832285
Abhay K. Keluskar <abhaykeluskar@live.com>
2023-05-15 16:13:16 +0530

    #2 password reset pages and API Integration

--- pages/password/reset.js
+++ pages/password/reset.js
@@ -1,93 +1,70 @@
 "use client"
 import { React, useState, useEffect } from 'react';
-import Head from 'next/head';
 import { useRouter } from 'next/router';
+import Head from 'next/head';
+import Link from 'next/link';
+import { useSearchParams } from 'next/navigation';
+import ErrorList from '../../components/errorList';
+import { verifyPasswordResetToken, resetPassword } from '../../services/password/password.services'
+import { disableSubmitButton, enableSubmitButton } from '../../utils/form.utils';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faKey, faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
-import styles from '../../styles/forgotPassword.module.scss';
+import styles from '../../styles/resetPassword.module.scss';
 
 export default function ResetPassword() {
-    const URL = process.env.NEXT_PUBLIC_HOST;
-    const [errorMessage, setErrorMessage] = useState("");
     const [visbilty, setvisibility] = useState(false);
-    const [hasError, setHasError] = useState(false);
-    const [password, setPassword] = useState("");
-    const [confirmPassword, setConfirmPassword] = useState("");
-    const router = useRouter()
+    const formErrors = [];
+    const [errors, setErrors] = useState([])
+    const [data, setData] = useState({ password: '', confirmPassword: '' })
+    const [isInvalidToken, SetIsInvalidToken] = useState(false)
+    const params = useSearchParams();
+    const router = useRouter();
+    const { push } = useRouter();
 
     const togglePasswordVisiblity = () => {
         setvisibility(visbilty ? false : true);
     };
 
     useEffect(() => {
-        var paramObj = router.query;
-        console.log(paramObj.token);
-        verifyToken(paramObj.token);
+        if (!router.isReady) return;
+        verifyToken();
+    }, [router.isReady]);
 
-    }, []);
+    const handleInput = ({ target }) => {
+        data[target.name] = target.value
+        setData(data)
+    }
 
-
-    const verifyToken = async (token) => {
+    const verifyToken = async () => {
         try {
-            const response = await fetch(URL + '/users/verify-password-reset/' + token, {
-                method: 'Get',
-                headers: {
-                    'Content-Type': 'application/JSON'
-                }
-            });
-            if (response.status == 200) {
-                console.log('ok')
-            } else {
-                setHasError(true);
-                setErrorMessage("Link Expired");
-            }
+            await verifyPasswordResetToken(params.get('token'))
         } catch (error) {
-            setHasError(true);
-            setErrorMessage('error');
+            SetIsInvalidToken(true)
         }
     }
 
-    const sendData = async (data) => {
-        try {
-            const response = await fetch(URL + '/users/password-reset', {
-                method: 'POST',
-                body: JSON.stringify(data),
-                headers: {
-                    'Content-Type': 'application/JSON'
-                }
-            });
-
-            const responseData = await response.json();
-            if (typeof (responseData.access_token) != 'undefined' && responseData.access_token != "") {
-                localStorage.setItem("accessToken", responseData.access_token);
-                document.location.pathname = '/';
-            } else {
-                setHasError(true);
-                setErrorMessage(responseData.message);
-            }
-
-        } catch (err) {
-            console.log(err);
-        }
-    }
-
-    const validateLoginInput = function (event) {
+    const hendleSubmit = async (event) => {
         event.preventDefault();
-        setHasError(false);
-        setErrorMessage('');
-        if (email != '') {
-            let data = {
-                "email": email
-            }
-            sendData(data);
-
+        const token = params.get('token');
+        formErrors.splice(0)
+        setErrors(formErrors)
+        if (formErrors.length > 0) {
+            setErrors(formErrors)
+            return
         } else {
-            setHasError(true);
-            setErrorMessage('Email or Password cannot be empty');
+            disableSubmitButton(event.target)
+            try {
+                await resetPassword(token, data);
+                setErrors('Your Password is reset please login again')
+                // push('/login');
+            } catch (error) {
+                setErrors(error.response.data.message);
+            }
+            enableSubmitButton(event.target)
         }
     }
     return (
-        <div className={`${styles.forgotPasswordContainer} container-fluid`}>
+        <div className={`${styles.resetPasswordContainer} container-fluid`}>
             <Head>
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
             </Head>
@@ -102,53 +79,55 @@ export default function ResetPassword() {
                             <p>Please enter new password</p>
                         </div>
                         <div className="col-sm-12 justify-content-md-center">
-                            <div className={`${styles.forgotPasswordCard} card`}>
+                            <div className={`${styles.resetPasswordCard} card`}>
                                 <div className="card-body p-0">
-                                    <div className={`${styles.forgotPasswordErrorMessageWrapper} ${hasError ? "" : styles.hide} `} >
-                                        <div className={`${styles.forgotPasswordErrorMessage}`}>{errorMessage}</div>
-                                    </div>
-                                    <form onSubmit={validateLoginInput}>
-                                        <div className="mb-3">
-                                            <label htmlFor="loginPassword" className="form-label">Password</label>
-                                            <div className={styles.innerInputIconWrapper}>
-                                                <i>
-                                                    <FontAwesomeIcon icon={faKey} />
-                                                </i>
-                                                <input type={visbilty ? "text" : "password"} className="form-control" placeholder="Password" value={password} onChange={(e) => { setPassword(e.target.value); setHasError(false); setErrorMessage(''); }} id="loginPassword" />
-                                                <i className={`${styles.toggleVisibilityWrapper}`} onClick={togglePasswordVisiblity}>
-                                                    <FontAwesomeIcon icon={visbilty ? faEyeSlash : faEye} />
-                                                </i>
+                                    <ErrorList errors={errors} />
+                                    {isInvalidToken ? (
+                                        <div> Token is invalid, Please check once or try again. </div>
+                                    ) : (
+                                        <form onSubmit={hendleSubmit}>
+                                            <div className="mb-3">
+                                                <label htmlFor="loginPassword" className="form-label">Password</label>
+                                                <div className={styles.innerInputIconWrapper}>
+                                                    <i>
+                                                        <FontAwesomeIcon icon={faKey} />
+                                                    </i>
+                                                    <input type={visbilty ? "text" : "password"} className="form-control" placeholder="Password" name="password" onChange={(e) => { handleInput(e); }} id="loginPassword" />
+                                                    <i className={`${styles.toggleVisibilityWrapper}`} onClick={togglePasswordVisiblity}>
+                                                        <FontAwesomeIcon icon={visbilty ? faEyeSlash : faEye} />
+                                                    </i>
 
+                                                </div>
                                             </div>
-                                        </div>
-                                        <div className="mb-3">
-                                            <label htmlFor="loginConfirmPassword" className="form-label">Confirm Password</label>
-                                            <div className={styles.innerInputIconWrapper}>
-                                                <i>
-                                                    <FontAwesomeIcon icon={faKey} />
-                                                </i>
-                                                <input type={visbilty ? "text" : "password"} className="form-control" placeholder='Password' value={confirmPassword} onChange={(e) => { setConfirmPassword(e.target.value); setHasError(false); setErrorMessage(''); }} id="loginConfirmPassword" />
-                                                <i className={`${styles.toggleVisibilityWrapper}`} onClick={togglePasswordVisiblity}>
-                                                    <FontAwesomeIcon icon={visbilty ? faEyeSlash : faEye} />
-                                                </i>
+                                            <div className="mb-3">
+                                                <label htmlFor="loginConfirmPassword" className="form-label">Confirm Password</label>
+                                                <div className={styles.innerInputIconWrapper}>
+                                                    <i>
+                                                        <FontAwesomeIcon icon={faKey} />
+                                                    </i>
+                                                    <input type={visbilty ? "text" : "password"} className="form-control" placeholder='Password' name="confirmPassword" onChange={(e) => { handleInput(e); }} id="loginConfirmPassword" />
+                                                    <i className={`${styles.toggleVisibilityWrapper}`} onClick={togglePasswordVisiblity}>
+                                                        <FontAwesomeIcon icon={visbilty ? faEyeSlash : faEye} />
+                                                    </i>
 
+                                                </div>
                                             </div>
-                                        </div>
-                                        <div className="d-grid gap-2">
-                                            <button type="submit" className="btn btn-primary">Reset Password</button>
-                                        </div>
-                                    </form>
+                                            <div className="d-grid gap-2">
+                                                <button type="submit" name='btn-submit' className="btn btn-primary">Reset Password</button>
+                                            </div>
+                                        </form>
+                                    )}
                                 </div>
                             </div>
                         </div>
                         <div className="col-sm-12">
                             <div className={`${styles.loginLinkWrapper}`}>
-                                {/* <p><Link href="/login">Login</Link></p> */}
+                                <p><Link href="/login">Login</Link></p>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className={`${styles.forgotPasswordBackground} col-md-6 .d-none .d-lg-block .d-xl-none`}>
+                <div className={`${styles.resetPasswordBackground} col-md-6 .d-none .d-lg-block .d-xl-none`}>
                 </div>
             </div>
         </div>
