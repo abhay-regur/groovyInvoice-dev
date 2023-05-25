5d565b4ab49911d5ebdd10d0eba31721c8e0ed8b
abhay-regur <abhay.keluskar@regur.net>
2023-05-25 20:15:35 +0530

    #27 Updated Function characted list

--- utils/genratePassword.utils.js
+++ utils/genratePassword.utils.js
@@ -1,6 +1,7 @@
 const generatePassword = () => {
     const length = Math.floor(Math.random() * 9) + 8; // Generates a random length between 8 and 16
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+~`|}{[]\:;?><,./-=';
+    // const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+~`|}{[]\:;?><,./-=';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@_';
     let generatedPassword = '';
 
     for (let i = 0; i < length; i++) {
