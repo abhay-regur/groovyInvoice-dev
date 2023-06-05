e0cd59ac6929d0ad1fd1b60270a8c4c719c3374e
abhay-regur <abhay.keluskar@regur.net>
2023-06-05 19:21:25 +0530

    #19 added redirection to Login or Dashboard on Login

--- app/page.js
+++ app/page.js
@@ -1,4 +1,15 @@
 'use client'
+import { redirect } from 'next/navigation';
+import { useEffect } from 'react';
+import { isLoggedIn } from '../services/auth.service';
 export default function GroovyInvoiceApp() {
+    useEffect(() => {
+        if (!isLoggedIn('user')) {
+            redirect('/login')
+        } else {
+            redirect('/dashboard');
+        }
+    });
+
     return (<></>)
 }
