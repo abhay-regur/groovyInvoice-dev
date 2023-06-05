fcfe8cc311ff7c48fcf50f2fd11998d67c6289c9
abhay-regur <abhay.keluskar@regur.net>
2023-06-05 19:24:04 +0530

    #19 Added link authorization on all pages.

--- app/(protectedPages)/layout.js
+++ app/(protectedPages)/layout.js
@@ -2,19 +2,26 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import Navbar from '../../components/navbar';
 import Footer from '../../components/footer';
+import { redirect } from 'next/navigation';
 import NavExpandedState from '../../context/NavState.context';
+import { isLoggedIn } from '../../services/auth.service';
 
 function Layout({ children }) {
-    return (
-        <>
-            <div className='pageContent'>
-                <NavExpandedState>
-                    <Navbar />
-                    {children}
-                </NavExpandedState>
-            </div>
-            <Footer />
-        </>)
+    if (!isLoggedIn('user')) {
+        redirect('/login')
+    } else {
+        return (
+            <>
+                <div className='pageContent'>
+                    <NavExpandedState>
+                        <Navbar />
+                        {children}
+                    </NavExpandedState>
+                </div>
+                <Footer />
+            </>)
+    }
+
 }
 
 export default Layout;
