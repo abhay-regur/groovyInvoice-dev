37c0d8f67600133d6620590ab476358039fbbe7c
pawar4912 <86315952+pawar4912@users.noreply.github.com>
2023-06-16 15:54:06 +0530

    #32 Linting error fixes

--- services/token.service.js
+++ services/token.service.js
@@ -1,12 +1,11 @@
 export const saveToken = (tokenName, accessToken) => {
-    return localStorage.setItem(tokenName, accessToken)
-  }
-  export const removeToken = (tokenName) => {
-    localStorage.removeItem(tokenName)
-  }
-  
-  export const getToken = (tokenName) => {
-    const token = localStorage.getItem(tokenName)
-    return token
-  }
-  
+  return localStorage.setItem(tokenName, accessToken)
+}
+export const removeToken = (tokenName) => {
+  localStorage.removeItem(tokenName)
+}
+
+export const getToken = (tokenName) => {
+  const token = localStorage.getItem(tokenName)
+  return token
+}
--- services/user.service.js
+++ services/user.service.js
@@ -3,9 +3,9 @@ import UserHTTPService from './user-http.service'
 const httpService = new UserHTTPService('user');
 
 export const createUser = async (data) => {
-    try {
-        return await httpService.post('users/create', data);
-    } catch (error) {
-        throw error;
-    }
+  try {
+    return await httpService.post('users/create', data);
+  } catch (error) {
+    throw error;
+  }
 }
