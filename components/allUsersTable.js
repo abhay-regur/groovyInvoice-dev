4193fa146a455c7c86bbd8eaa475710e0caad54c
abhay-regur <abhay.keluskar@regur.net>
2023-06-19 20:50:17 +0530

    [#19]  Added User Updated API support.

--- components/allUsersTable.js
+++ components/allUsersTable.js
@@ -1,16 +1,20 @@
 "use client"
 import styles from '../styles/user.module.scss';
-import { useRef } from 'react';
+import { useContext, useRef } from 'react';
 import 'datatables.net-dt/js/dataTables.dataTables';
 import Image from 'next/image';
 import ReactDOM from "react-dom/client";
 import defaultProfile from '../public/images/profile_Default.png';
 import { userActivate, userDeactivate } from '../services/user.service';
+import { ToastMsgContext } from '../context/ToastMsg.context';
 import FaPen from '../assets/icons/faPen.svg';
 import 'datatables.net-dt/css/jquery.dataTables.min.css';
 import ServerSideDataTables from './serverSideDataTable';
+import Link from 'next/link';
+import Toast from './toast';
 
 const AllUserTable = () => {
+    const { setToastList } = useContext(ToastMsgContext)
 
     const dtRef = useRef();
 
@@ -23,9 +27,9 @@ const AllUserTable = () => {
                         <div className={`${styles.companyUserTableCustomerName}`}>
                             {row.firstName + ' ' + row.lastName}
                         </div>
-                        <div className={`${styles.companyUserTableCustomerEdit} ps-2`}>
+                        <Link className={`${styles.companyUserTableCustomerEdit} ps-2`} href={`/users/update/${row.id}`}>
                             <FaPen />
-                        </div>
+                        </Link>
                     </span >
                 </div >
             </>
@@ -36,7 +40,7 @@ const AllUserTable = () => {
         return (
             <span className={styles.companyUserTableExtraFunction}>
                 <div className={`${styles.companyUserActiveUserSwitchWrapper} form-check form-switch align-items-center d-flex`}>
-                    <input className={`${styles.companyUserActiveUserSwitch} form-check-input`} type="checkbox" role="switch" onChange={() => { handlechange(rowData.id, rowData.active) }} id="flexSwitchCheckDefault" checked={rowData.active} />
+                    <input className={`${styles.companyUserActiveUserSwitch} form-check-input`} type="checkbox" role="switch" onChange={() => { handlechange(rowData) }} id="flexSwitchCheckDefault" checked={rowData.active} />
                 </div>
             </span>
         );
@@ -75,37 +79,44 @@ const AllUserTable = () => {
         ],
     }
 
-    const handlechange = async (id, active) => {
+    const handlechange = async (rowData) => {
         var result = '';
-        if (active) {
-            result = await userDeactivate(id);
+        if (rowData.active) {
+            result = await userDeactivate(rowData.id);
         } else {
-            result = await userActivate(id);
+            result = await userActivate(rowData.id);
         }
-        console.log(result.status);
+
         if (result.status == 200) {
-            dtRef.current.reload()
+            dtRef.current.reload();
+            setToastList([{
+                id: Math.floor((Math.random() * 101) + 1),
+                title: rowData.firstName + ' ' + rowData.lastName + ' status updated',
+                description: result.data.message,
+            }])
         } else {
             console.log(result.data.message);
         }
     }
     return (
-
-        <div className={`row`}>
-            <div className={`${styles.comapanyInoviceUserTableWrapper} col-sm-12 p-0`}>
-                <ServerSideDataTables ref={dtRef} id="manage-user--table" {...dtOptions} className={`table responsive`}>
-                    <thead>
-                        <tr>
-                            <th scope="col" className="ps-3" data-priority="1" >User Name</th>
-                            <th scope="col" className="ps-3" data-priority="99">Contact Number</th>
-                            <th scope="col" className="ps-3" data-priority="99">Email Address</th>
-                            <th scope="col" className="ps-3" data-priority="2" >Active</th>
-                        </tr>
-                    </thead>
-                    <tbody></tbody>
-                </ServerSideDataTables>
-            </div>
-        </div >
+        <>
+            <div className={`row`}>
+                <div className={`${styles.comapanyInoviceUserTableWrapper} col-sm-12 p-0`}>
+                    <ServerSideDataTables ref={dtRef} id="manage-user--table" {...dtOptions} className={`table responsive`}>
+                        <thead>
+                            <tr>
+                                <th scope="col" className="ps-3" data-priority="1" >User Name</th>
+                                <th scope="col" className="ps-3" data-priority="99">Contact Number</th>
+                                <th scope="col" className="ps-3" data-priority="99">Email Address</th>
+                                <th scope="col" className="ps-3" data-priority="2" >Active</th>
+                            </tr>
+                        </thead>
+                        <tbody></tbody>
+                    </ServerSideDataTables>
+                </div>
+            </div >
+            <Toast />
+        </>
 
     )
 
--- services/user.service.js
+++ services/user.service.js
@@ -5,15 +5,23 @@ const httpService = new UserHTTPService('user');
 export const userActivate = async (id) => {
     try {
         return await httpService.patch('users/activate/' + id);
-    } catch (error) {
-        throw error;
-    }
+    } catch (error) { throw error; }
 }
 
 export const userDeactivate = async (id) => {
     try {
         return await httpService.patch('users/deactivate/' + id);
-    } catch (error) {
-        throw error;
-    }
+    } catch (error) { throw error; }
+}
+
+export const userDetails = async (id) => {
+    try {
+        return await httpService.get('users/' + id)
+    } catch (error) { throw (error) }
+}
+
+export const updateUserDetails = async (data) => {
+    try {
+        return await httpService.put('users/update/' + data.id, data);
+    } catch (error) { throw (error) }
 }
