6dc3e3c1157de09e1c558a983609d479a618c080
abhay-regur <abhay.keluskar@regur.net>
2023-07-11 19:17:06 +0530

    [#37] Loading is now Reusable.

--- app/(protectedPages)/users/loading.js
+++ app/(protectedPages)/users/loading.js
@@ -2,125 +2,47 @@ import styles from '../../../styles/loading.module.scss';
 import defaultProfile from '../../../public/images/default_profile_icon.png';
 import Image from 'next/image';
 export default function Loading(props) {
+    const cols = [];
+    const rows = [];
+    const tdprofile = (
+        <td key={0}>
+            <div className={`${styles.companyUserTableCustomerImage}`}>
+                <Image src={defaultProfile} alt="Picture of the author" width={42} height={42} />
+                <span className={`${styles.companyUserTableCustomerLoadingWrapper}`} >
+                    <div className={`${styles.companyUserTableCustomerLoader}`}>
+                    </div>
+                </span >
+            </div >
+        </td>
+    )
+    const td = function (id) {
+        return (
+            <td key={id}>
+                <span className={`${styles.companyUserTableCustomerLoadingWrapper}`} >
+                    <div className={`${styles.companyUserTableCustomerLoader}`}>
+                    </div>
+                </span >
+            </td>
+        )
+    }
+    const tr = function (id) {
+        return (
+            <tr key={id}>
+                {props.isProfile ? tdprofile : td(0)}
+                {cols}
+            </tr>
+        )
+    }
+    for (var i = 1; i < props.columnLength; i++) {
+        cols.push(td(i))
+    }
+    for (var i = 0; i < props.rowsLength; i++) {
+        rows.push(tr(i))
+    }
     return (
         <>
             <tbody className={props.isLoading ? "" : "hide"}>
-                <tr>
-                    <td>
-                        <div className={`${styles.companyUserTableCustomerImage}`}>
-                            <Image src={defaultProfile} alt="Picture of the author" width={42} height={42} />
-                            <span className={`${styles.companyUserTableCustomerLoadingWrapper}`} >
-                                <div className={`${styles.companyUserTableCustomerLoader}`}>
-                                </div>
-                            </span >
-                        </div >
-                    </td>
-                    <td>
-                        <span className={`${styles.companyUserTableCustomerLoadingWrapper}`} >
-                            <div className={`${styles.companyUserTableCustomerLoader}`}>
-                            </div>
-                        </span >
-                    </td>
-                    <td>
-                        <span className={`${styles.companyUserTableCustomerLoadingWrapper}`} >
-                            <div className={`${styles.companyUserTableCustomerLoader}`}>
-                            </div>
-                        </span >
-                    </td>
-                    <td>
-                        <span className={`${styles.companyUserTableCustomerLoadingWrapper}`} >
-                            <div className={`${styles.companyUserTableCustomerLoader}`}>
-                            </div>
-                        </span >
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <div className={`${styles.companyUserTableCustomerImage}`}>
-                            <Image src={defaultProfile} alt="Picture of the author" width={42} height={42} />
-                            <span className={`${styles.companyUserTableCustomerLoadingWrapper}`} >
-                                <div className={`${styles.companyUserTableCustomerLoader}`}>
-                                </div>
-                            </span >
-                        </div >
-                    </td>
-                    <td>
-                        <span className={`${styles.companyUserTableCustomerLoadingWrapper}`} >
-                            <div className={`${styles.companyUserTableCustomerLoader}`}>
-                            </div>
-                        </span >
-                    </td>
-                    <td>
-                        <span className={`${styles.companyUserTableCustomerLoadingWrapper}`} >
-                            <div className={`${styles.companyUserTableCustomerLoader}`}>
-                            </div>
-                        </span >
-                    </td>
-                    <td>
-                        <span className={`${styles.companyUserTableCustomerLoadingWrapper}`} >
-                            <div className={`${styles.companyUserTableCustomerLoader}`}>
-                            </div>
-                        </span >
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <div className={`${styles.companyUserTableCustomerImage}`}>
-                            <Image src={defaultProfile} alt="Picture of the author" width={42} height={42} />
-                            <span className={`${styles.companyUserTableCustomerLoadingWrapper}`} >
-                                <div className={`${styles.companyUserTableCustomerLoader}`}>
-                                </div>
-                            </span >
-                        </div >
-                    </td>
-                    <td>
-                        <span className={`${styles.companyUserTableCustomerLoadingWrapper}`} >
-                            <div className={`${styles.companyUserTableCustomerLoader}`}>
-                            </div>
-                        </span >
-                    </td>
-                    <td>
-                        <span className={`${styles.companyUserTableCustomerLoadingWrapper}`} >
-                            <div className={`${styles.companyUserTableCustomerLoader}`}>
-                            </div>
-                        </span >
-                    </td>
-                    <td>
-                        <span className={`${styles.companyUserTableCustomerLoadingWrapper}`} >
-                            <div className={`${styles.companyUserTableCustomerLoader}`}>
-                            </div>
-                        </span >
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <div className={`${styles.companyUserTableCustomerImage}`}>
-                            <Image src={defaultProfile} alt="Picture of the author" width={42} height={42} />
-                            <span className={`${styles.companyUserTableCustomerLoadingWrapper}`} >
-                                <div className={`${styles.companyUserTableCustomerLoader}`}>
-                                </div>
-                            </span >
-                        </div >
-                    </td>
-                    <td>
-                        <span className={`${styles.companyUserTableCustomerLoadingWrapper}`} >
-                            <div className={`${styles.companyUserTableCustomerLoader}`}>
-                            </div>
-                        </span >
-                    </td>
-                    <td>
-                        <span className={`${styles.companyUserTableCustomerLoadingWrapper}`} >
-                            <div className={`${styles.companyUserTableCustomerLoader}`}>
-                            </div>
-                        </span >
-                    </td>
-                    <td>
-                        <span className={`${styles.companyUserTableCustomerLoadingWrapper}`} >
-                            <div className={`${styles.companyUserTableCustomerLoader}`}>
-                            </div>
-                        </span >
-                    </td>
-                </tr>
+                {rows}
             </tbody>
         </>
     )
