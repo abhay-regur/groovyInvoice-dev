d228c6baf45ed3a984e2d6816355a77fddbc7df3
abhay-regur <abhay.keluskar@regur.net>
2023-08-18 17:24:34 +0530

    [#45] Added Config page and payment terms CRUD

---
+++ services/paymentTerms.service.js
@@ -0,0 +1,32 @@
+import UserHTTPService from './user-http.service';
+const httpService = new UserHTTPService('user');
+
+export const getPaymentTerms = async () => {
+    try {
+        return await httpService.get('payments-term');
+    } catch (error) { throw error; }
+}
+
+export const createPaymentTerms = async (data) => {
+    try {
+        return await httpService.post('payments-term/create', data);
+    } catch (error) {
+        throw error;
+    }
+}
+
+export const updatePaymentTerms = async (id, data) => {
+    try {
+        return await httpService.put('payments-term/' + id, data);
+    } catch (error) {
+        throw error;
+    }
+}
+
+export const deletePaymentTerms = async (id) => {
+    try {
+        return await httpService.delete('payments-term/' + id);
+    } catch (error) {
+        throw error;
+    }
+}
