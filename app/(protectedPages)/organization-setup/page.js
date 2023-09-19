c3d86d273162c0546d95611e211f599b791b3128
abhay-regur <abhay.keluskar@regur.net>
2023-09-19 16:30:17 +0530

    [#45] Added Update-Organistation details page

---
+++ app/(protectedPages)/organization-setup/page.js
@@ -0,0 +1,9 @@
+import OrganizationSetupForm from "./formComponent"
+export const metadata = {
+    title: 'New Organization Setup',
+}
+export default function OrganizationSetup() {
+    return (
+        <OrganizationSetupForm />
+    )
+}
