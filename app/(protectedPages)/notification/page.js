538dd97ecdb97e66e8236b5417f523c57f78588d
abhay-regur <abhay.keluskar@regur.net>
2023-12-22 19:54:55 +0530

    [#46] Notification Page first push

---
+++ app/(protectedPages)/notification/page.js
@@ -0,0 +1,8 @@
+import NotificationComponent from "./notificationComponent";
+export const metadata = {
+    title: 'Notifications',
+};
+
+export default function Notifications() {
+    return (<NotificationComponent />)
+}
