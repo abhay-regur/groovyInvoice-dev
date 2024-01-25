4e9b63df09ef82d859b1ef0587205aa58fe0bc0b
abhay-regur <abhay.keluskar@regur.net>
2024-01-25 17:57:54 +0530

    [#49] Change: Removed ID from breadcrumbs.

--- components/common/breadcrumb.js
+++ components/common/breadcrumb.js
@@ -1,16 +1,23 @@
-import { usePathname } from 'next/navigation';
+import { usePathname, useParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import styles from '@/styles/breadcrumb.module.scss';
 
 export default function Breadcrumb() {
     const pathname = usePathname();
+    const param = useParams();
     const [breadcrumb, setBreadcrumb] = useState([]);
 
     useEffect(() => {
         const asPathWithoutQuery = pathname.split("?")[0];
         const asPathNestedRoutes = asPathWithoutQuery.split("/").filter(v => v.length > 0);
+        if (asPathNestedRoutes.indexOf('update') > -1) {
+            asPathNestedRoutes.pop()
+        }
         const crumblist = asPathNestedRoutes.map((subpath, idx) => {
             const href = "/" + asPathNestedRoutes.slice(0, idx + 1).join("/");
+            if (param.id != undefined && subpath == param.id) {
+                subpath = 'view';
+            }
             const title = subpath.charAt(0).toUpperCase() + subpath.slice(1);
             return { href, title };
         })
@@ -23,7 +30,7 @@ export default function Breadcrumb() {
             <ol className="breadcrumbs ps-0">
                 {
                     breadcrumb.map((crumb, idx) => {
-                        if (idx != (breadcrumb.length - 1) && crumb.title != 'Update') {
+                        if (idx != (breadcrumb.length - 1)) {
                             return <li key={idx} className={`${styles.breadcrumbItem} breadcrumb_item`}><a href={crumb.href}>{crumb.title}</a></li>
                         } else {
                             return <li key={idx} className={`${styles.breadcrumbItem} ${styles.currentPageWrapper} breadcrumb_item`}><span className={`${styles.currentPage}`}>{crumb.title}</span></li>
