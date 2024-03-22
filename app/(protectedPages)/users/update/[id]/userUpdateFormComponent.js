149050a02a756601cd426715332d9450c467c6ca
pawar4912 <86315952+pawar4912@users.noreply.github.com>
2024-03-22 13:36:30 +0530

    #73 users form optimisations

--- app/(protectedPages)/users/update/[id]/userUpdateFormComponent.js
+++ app/(protectedPages)/users/update/[id]/userUpdateFormComponent.js
@@ -3,17 +3,14 @@ import { useEffect, useState } from 'react';
 import { useContext } from 'react';
 import { useParams, useRouter } from 'next/navigation';
 import { userDetails, updateUserDetails } from '@/services/user.service';
-import FaSave from '@/assets/icons/faSave.svg';
-import FaCircleXmark from '@/assets/icons/faCircleXmark.svg';
 import styles from "@/styles/userForm.module.scss";
-import ErrorList from '@/components/errorList';
 import { NavExpandedState } from '@/context/NavState.context';
 import { ToastMsgContext } from '@/context/ToastMsg.context';
 import { genrateErrorMessage } from '@/utils/errorMessageHandler.utils.js';
 import Breadcrumb from '@/components/common/breadcrumb';
-import Link from 'next/link';
 import Loading from './loading.js';
-import { disableElement, enableElement } from '@/utils/form.utils.js';
+import { disableSubmitButton, enableSubmitButton } from '@/utils/form.utils.js';
+import UserForm from '@/components/users/form';
 
 export default function UserUpdateFormComponent() {
     const { id } = useParams();
@@ -21,7 +18,6 @@ export default function UserUpdateFormComponent() {
     const { navExpandedState } = useContext(NavExpandedState);
     const { setToastList } = useContext(ToastMsgContext)
     const [errors, setErrors] = useState([]);
-    const [isSubmit, setIsSubmit] = useState(false);
 
     const [isLoading, setIsLoading] = useState(true)
 
@@ -33,23 +29,8 @@ export default function UserUpdateFormComponent() {
         active: false
     })
 
-    const handleInput = ({ target }) => {
-        if (target.name == 'phone') {
-            data[target.name] = (target.value == '' ? '' : (target.value).match(/[0-9]+/g)[0]);
-        } else {
-            data[target.name] = target.value;
-        }
-        let temp = Object.assign({}, data)
-        setData(temp)
-    }
-
-    const handleSwitchChange = async ({ target }) => {
-        data[target.name] = !data[target.name]
-        let temp = Object.assign({}, data)
-        setData(temp)
-    }
-
     useEffect(() => {
+        setIsLoading(true);
         getUserDetails();
     }, []);
 
@@ -58,19 +39,19 @@ export default function UserUpdateFormComponent() {
         try {
             const result = await userDetails(id);
             setData(result.data);
-            setIsLoading(false);
         } catch (error) {
             if (error.response != undefined && error.response.status == 404) {
                 replace('/404');
             } else {
                 setErrors(genrateErrorMessage(error, '', setToastList));
-                setIsLoading(false);
             }
         }
+        setIsLoading(false);
     }
 
-    const handleSaveClick = async ({currentTarget}) => {
-        disableElement(currentTarget)
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        disableSubmitButton(e.target)
         setErrors([]);
         try {
             const result = await updateUserDetails(id, data);
@@ -82,11 +63,8 @@ export default function UserUpdateFormComponent() {
         } catch (error) {
             setErrors(genrateErrorMessage(error, '', setToastList));
         }
-        enableElement(currentTarget)
-    }
-
-    const handleCancelClick = () => {
-        setErrors([]);
+        enableSubmitButton(e.target)
+        getUserDetails();
     }
 
     return (
@@ -95,82 +73,16 @@ export default function UserUpdateFormComponent() {
                 <Breadcrumb styles={styles} />
             </div>
             <div className="container-fluid">
-                <div className={`${styles.card} card`}>
-                    <div className={`${styles.cardBody} card-body`}>
-                        <h4 className={`${styles.cardTitle} card-title`}>Update User</h4>
-
-                        <hr />
-                        {isLoading ?
-                            <Loading /> :
-                            <>
-                                <div className={`${styles.companyUserActiveUserWrapper} mb-4 row`}>
-                                    <div className="d-flex align-items-center col-12 col-lg-2 col-xl-2">
-                                        <label className="">Active</label>
-                                    </div>
-                                    <div className="col-12 col-lg-6 col-xl-6">
-                                        <div className={`${styles.companyUserActiveUserSwitchWrapper} form-check form-switch align-items-center d-flex`}>
-                                            <input className={`${styles.companyUserActiveUserSwitch} form-check-input`} type="checkbox" role="switch" id="flexSwitchCheckDefault" name='active' checked={data.active} onChange={handleSwitchChange} />
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <ErrorList errors={errors} />
-
-                                <div className={`${styles.companyInvoiceUserNameWrapper} mb-0 mb-md-4 row`}>
-                                    <div className="d-flex align-items-center col-12 col-lg-2 col-xl-2">
-                                        <label className="">Name</label>
-                                    </div>
-                                    <div className="col-12 col-lg-3 col-xl-3">
-                                        <input type="text" className={`${styles.companyInvoiceUserFirstName} form-control`} name='firstName' value={data.firstName == null ? '' : data.firstName} placeholder='First Name' onChange={handleInput} />
-                                    </div>
-                                    <div className="col-12 col-lg-3 col-xl-3">
-                                        <input type="text" className={`${styles.companyInvoiceUserLastName} form-control`} value={data.lastName == null ? '' : data.lastName} name='lastName' placeholder='Last Name' onChange={handleInput} />
-                                    </div>
-                                </div>
-
-                                <div className={`${styles.companyInvoiceUserEmailWrapper} mb-4 row`}>
-                                    <div className="d-flex align-items-center col-12 col-lg-2 col-xl-2">
-                                        <label className={`${styles.companyInvoiceUserEmailLabel}`}>Email</label>
-                                    </div>
-                                    <div className="col-12 col-lg-6 col-xl-6 d-flex align-items-center">
-                                        <input type="email" className="form-control" id="companyInvoiceUserEmail" value={data.email} name='email' placeholder='Email' disabled />
-                                    </div>
-                                </div>
-
-                                <div className={`${styles.companyInvoiceUserPhone} mb-4 row`}>
-                                    <div className="d-flex align-items-center col-12 col-lg-2 col-xl-2">
-                                        <label className={`${styles.companyInvoiceUserPhoneLabel}`}>Phone</label>
-                                    </div>
-
-                                    <div className="col-12 col-lg-6 col-xl-6 d-flex align-items-center">
-                                        <input type="tel" minLength={4} maxLength={13} className={`${styles.companyInvoiceUserMobile} form-control`} value={data.cellNumber} name='cellNumber' placeholder='Mobile' onChange={handleInput} />
-                                    </div>
-                                </div>
-
-                                <div className={`${styles.companyInvoiceFormButtonsWrapper} row`}>
-                                    <div className="col-12 col-sm-10 col-md-8 col-lg-7 col-xl-5">
-                                        <div className="d-flex gap-3">
-                                            <button className={`${styles.companyInvoiceSavenSendButton} btn blue`} name='btn-submit' onClick={(e) => { handleSaveClick(e) }}>
-                                                <span>
-                                                    <i><FaSave /></i> Save
-                                                </span>
-                                            </button>
-                                            <Link href={'/users/'}>
-                                                <button className={`${styles.companyInvoiceCancelButton} btn blueOutline`} onClick={() => { handleCancelClick() }}>
-                                                    <span>
-                                                        <i><FaCircleXmark /></i>
-                                                        Cancel
-                                                    </span>
-                                                </button>
-
-                                            </Link>
-                                        </div>
-                                    </div>
-                                </div>
-                            </>
-                        }
-                    </div>
-                </div>
+                {isLoading ?
+                    <Loading /> :
+                    <UserForm
+                        handleSubmit={handleSubmit}
+                        data={data}
+                        setData={setData}
+                        errors={errors}
+                        label="Update User"
+                    />
+                }
             </div>
         </main>
     )
