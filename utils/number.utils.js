11a08d9a3583c1bc271a9e2edb0ac6bbd2f086ce
abhay-regur <abhay.keluskar@regur.net>
2024-04-09 18:29:42 +0530

    [#78] Bug: Fixed incorrect words getting returned

--- utils/number.utils.js
+++ utils/number.utils.js
@@ -1,19 +1,93 @@
 const convertNumberToWord = (numberInput) => {
-    let oneToTwenty = ['','one ','two ','three ','four ', 'five ','six ','seven ','eight ','nine ','ten ',
-    'eleven ','twelve ','thirteen ','fourteen ','fifteen ','sixteen ','seventeen ','eighteen ','nineteen '];
-    let tenth = ['', '', 'twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
+    {
+        // let oneToTwenty = ['', 'one ', 'two ', 'three ', 'four ', 'five ', 'six ', 'seven ', 'eight ', 'nine ', 'ten ',
+        //     'eleven ', 'twelve ', 'thirteen ', 'fourteen ', 'fifteen ', 'sixteen ', 'seventeen ', 'eighteen ', 'nineteen '];
+        // let tenth = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
 
-    let num = ('0000000'+ numberInput).slice(-7).match(/^(\d{1})(\d{1})(\d{2})(\d{1})(\d{2})$/);
-    if(!num) return;
+        // let num = ('0000000' + numberInput).slice(-7).match(/^(\d{1})(\d{1})(\d{2})(\d{1})(\d{2})$/);
+        // if (!num) return;
 
-    let outputText = num[1] != 0 ? (oneToTwenty[Number(num[1])] || `${tenth[num[1][0]]} ${oneToTwenty[num[1][1]]}` )+' million ' : ''; 
-  
-    outputText +=num[2] != 0 ? (oneToTwenty[Number(num[2])] || `${tenth[num[2][0]]} ${oneToTwenty[num[2][1]]}` )+'hundred ' : ''; 
-    outputText +=num[3] != 0 ? (oneToTwenty[Number(num[3])] || `${tenth[num[3][0]]} ${oneToTwenty[num[3][1]]}`)+' thousand ' : ''; 
-    outputText +=num[4] != 0 ? (oneToTwenty[Number(num[4])] || `${tenth[num[4][0]]} ${oneToTwenty[num[4][1]]}`) +'hundred ': ''; 
-    outputText +=num[5] != 0 ? (oneToTwenty[Number(num[5])] || `${tenth[num[5][0]]} ${oneToTwenty[num[5][1]]} `) : ''; 
+        // let outputText = num[1] != 0 ? (oneToTwenty[Number(num[1])] || `${tenth[num[1][0]]} ${oneToTwenty[num[1][1]]}`) + ' million ' : '';
 
-    return outputText;
+        // outputText += num[2] != 0 ? (oneToTwenty[Number(num[2])] || `${tenth[num[2][0]]} ${oneToTwenty[num[2][1]]}`) + 'hundred ' : '';
+        // outputText += num[3] != 0 ? (oneToTwenty[Number(num[3])] || `${tenth[num[3][0]]} ${oneToTwenty[num[3][1]]}`) + ' thousand ' : '';
+        // outputText += num[4] != 0 ? (oneToTwenty[Number(num[4])] || `${tenth[num[4][0]]} ${oneToTwenty[num[4][1]]}`) + 'hundred ' : '';
+        // outputText += num[5] != 0 ? (oneToTwenty[Number(num[5])] || `${tenth[num[5][0]]} ${oneToTwenty[num[5][1]]} `) : '';
+
+        // return outputText;
+    }
+
+    var fraction = Math.round(frac(numberInput) * 100);
+    var f_text = "";
+
+    if (fraction > 0) {
+        f_text = "POINT " + convert_number(fraction);
+    }
+
+    return convert_number(numberInput) + " " + f_text + " ONLY";
+}
+
+function frac(f) {
+    return f % 1;
+}
+
+function convert_number(number) {
+    if ((number < 0) || (number > 999999999)) {
+        return "NUMBER OUT OF RANGE!";
+    }
+    var Gn = Math.floor(number / 10000000);/* Crore */
+    number -= Gn * 10000000;
+    var kn = Math.floor(number / 100000);/* lakhs */
+    number -= kn * 100000;
+    var Hn = Math.floor(number / 1000);/* thousand */
+    number -= Hn * 1000;
+    var Dn = Math.floor(number / 100);/* Tens (deca) */
+    number = number % 100;/* Ones */
+    var tn = Math.floor(number / 10);
+    var one = Math.floor(number % 10);
+    var res = "";
+
+    if (Gn > 0) {
+        res += (convert_number(Gn) + " CRORE");
+    }
+    if (kn > 0) {
+        res += (((res == "") ? "" : " ") +
+            convert_number(kn) + " LAKH");
+    }
+    if (Hn > 0) {
+        res += (((res == "") ? "" : " ") +
+            convert_number(Hn) + " THOUSAND");
+    }
+
+    if (Dn) {
+        res += (((res == "") ? "" : " ") +
+            convert_number(Dn) + " HUNDRED");
+    }
+
+
+    var ones = Array("", "ONE", "TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN", "EIGHT", "NINE", "TEN", "ELEVEN", "TWELVE", "THIRTEEN", "FOURTEEN", "FIFTEEN", "SIXTEEN", "SEVENTEEN", "EIGHTEEN", "NINETEEN");
+    var tens = Array("", "", "TWENTY", "THIRTY", "FOURTY", "FIFTY", "SIXTY", "SEVENTY", "EIGHTY", "NINETY");
+
+    if (tn > 0 || one > 0) {
+        if (!(res == "")) {
+            res += " AND ";
+        }
+        if (tn < 2) {
+            res += ones[tn * 10 + one];
+        }
+        else {
+
+            res += tens[tn];
+            if (one > 0) {
+                res += ("-" + ones[one]);
+            }
+        }
+    }
+
+    if (res == "") {
+        res = "ZERO";
+    }
+    return res;
 }
 
 export {
